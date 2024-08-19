dad4bf98d4b21b48a5729299e097d2d23f7fe2ad
unknown <pedrohenriquefdasilva14@gmail.com>
2024-08-19 19:38:48 -0300

    feat: Create Account entity

---
+++ wallet/wallet/src/main/java/com/swiss/wallet/entity/Account.java
@@ -0,0 +1,56 @@
+package com.swiss.wallet.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double value = 00.00;
+
+    @OneToOne
+    @JoinColumn(name = "idUser")
+    private UserEntity user;
+
+    public Account(Long id, Double value, UserEntity user) {
+        this.id = id;
+        this.value = value;
+        this.user = user;
+    }
+
+    public Account(Double value, UserEntity user) {
+        this.value = value;
+        this.user = user;
+    }
+
+    public Account() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+}
