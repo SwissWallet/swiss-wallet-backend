dc56729ce88259ab31eb6e0a46b14f6383004aca
unknown <pedrohenriquefdasilva14@gmail.com>
2024-08-19 20:15:21 -0300

    feat: create UserController controller

---
+++ wallet/wallet/src/main/java/com/swiss/wallet/UserController.java
@@ -0,0 +1,16 @@
+package com.swiss.wallet;
+
+import com.swiss.wallet.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v3/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
