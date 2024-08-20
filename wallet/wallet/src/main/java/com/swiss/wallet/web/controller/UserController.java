6f2e86dc09285a2b7d4ce5ce98c4fdd8755e3b2a
unknown <pedrohenriquefdasilva14@gmail.com>
2024-08-20 19:55:58 -0300

    feat: create saveUser  documentation

--- wallet/wallet/src/main/java/com/swiss/wallet/web/controller/UserController.java
+++ wallet/wallet/src/main/java/com/swiss/wallet/web/controller/UserController.java
@@ -6,6 +6,12 @@ import com.swiss.wallet.service.UserService;
 import com.swiss.wallet.web.dto.UserAddressCreateDto;
 import com.swiss.wallet.web.dto.UserPasswordRecoveryDto;
 import com.swiss.wallet.web.dto.UserResponseDto;
+import com.swiss.wallet.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +19,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Users", description = "Contains all operations related to resources for registering, editing and reading a user.")
 @RestController
 @RequestMapping("/api/v3/users")
 public class UserController {
@@ -23,13 +30,21 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Create a new user", description = "Feature to create a new user",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Resource created successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Email user already registered in the system",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Resource not processed due to invalid input data",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<UserResponseDto> saveUser(@RequestBody @Valid UserAddressCreateDto userAddressCreateDto){
         UserEntity user = userService.saveUser(userAddressCreateDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(UserResponseDto.toUserResponse(user));
     }
 
-    //Method to generate forgotten code the password
     @PostMapping("/recover-password")
     public ResponseEntity<String> recoverPassword(@RequestParam String username){
         String code = userService.recoverPassword(username);
