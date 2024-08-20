d37978d7048d69094da2509ccc7c32585d152c87
unknown <pedrohenriquefdasilva14@gmail.com>
2024-08-19 21:36:10 -0300

    update: refactor UserLoginDto dto

--- wallet/wallet/src/main/java/com/swiss/wallet/service/UserService.java
+++ wallet/wallet/src/main/java/com/swiss/wallet/service/UserService.java
@@ -9,6 +9,7 @@ import com.swiss.wallet.repository.IAccountRepository;
 import com.swiss.wallet.repository.IAddressRepository;
 import com.swiss.wallet.repository.IUserRepository;
 import com.swiss.wallet.web.dto.UserAddressCreateDto;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,7 @@ public class UserService {
     private final IUserRepository userRepository;
     private final IAddressRepository addressRepository;
     private final IAccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(IUserRepository userRepository, IAddressRepository addressRepository, IAccountRepository accountRepository) {
         this.userRepository = userRepository;
@@ -33,7 +35,9 @@ public class UserService {
             throw new UserUniqueViolationException(String.format("A user with this cpf= %s already exists. Please use a different cpf.", user.getCpf()));
         }
         user.setAddress(address);
+        user.setPassword(passwordEncoder.encode(userAddressCreateDto.user().password()));
         user = userRepository.save(user);
+
 
         Account account = new Account();
         account.setUser(user);
