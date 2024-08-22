6254f0d6596aa523143b981a0e45874160609426
unknown <pedrohenriquefdasilva14@gmail.com>
2024-08-22 20:33:51 -0300

    feat: implement getAccountByUserCurrent in AccountController

--- wallet/wallet/src/main/java/com/swiss/wallet/web/controller/AccountController.java
+++ wallet/wallet/src/main/java/com/swiss/wallet/web/controller/AccountController.java
@@ -1,6 +1,13 @@
 package com.swiss.wallet.web.controller;
 
+import com.swiss.wallet.entity.Account;
+import com.swiss.wallet.jwt.JwtUserDetails;
 import com.swiss.wallet.service.AccountService;
+import com.swiss.wallet.web.dto.AccountResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +19,12 @@ public class AccountController {
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
+    }
+
+    @GetMapping("/current")
+    @PreAuthorize("hasRole('CLIENT')")
+    public ResponseEntity<AccountResponseDto> getAccountUserCurrent(@AuthenticationPrincipal JwtUserDetails userDetails){
+        Account account = accountService.findByUserId(userDetails.getId());
+        return ResponseEntity.ok().body(AccountResponseDto.toAccountResponseDto(account));
     }
 }
