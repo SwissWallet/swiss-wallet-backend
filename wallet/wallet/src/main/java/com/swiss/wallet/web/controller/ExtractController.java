0dcf210aafe19cdba229af95c8bc8922ff624265
unknown <pedrohenriquefdasilva14@gmail.com>
2024-08-23 19:34:52 -0300

    feat: added documentation to findExtractByUserLogged method

--- wallet/wallet/src/main/java/com/swiss/wallet/web/controller/ExtractController.java
+++ wallet/wallet/src/main/java/com/swiss/wallet/web/controller/ExtractController.java
@@ -4,6 +4,14 @@ import com.swiss.wallet.entity.Extract;
 import com.swiss.wallet.jwt.JwtUserDetails;
 import com.swiss.wallet.service.ExtractService;
 import com.swiss.wallet.web.dto.ExtractResponseDto;
+import com.swiss.wallet.web.dto.UserResponseDto;
+import com.swiss.wallet.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Extracts", description = "Contains all operations related to resources for registering, editing and reading a extract.")
 @RestController
 @RequestMapping("/api/v3/extracts")
 public class ExtractController {
@@ -23,6 +32,14 @@ public class ExtractController {
         this.extractService = extractService;
     }
 
+    @Operation(summary = "Recover account logged in user", description = "Request requires a Bearer Token. Restricted access to CLIENT",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Resource retrieved successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "403", description = "User not allowed to access this resource",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            })
     @GetMapping("/current")
     @PreAuthorize("hasRole('CLIENT')")
     public ResponseEntity<?> findExtractByUserLogged(@AuthenticationPrincipal JwtUserDetails userDetails){
