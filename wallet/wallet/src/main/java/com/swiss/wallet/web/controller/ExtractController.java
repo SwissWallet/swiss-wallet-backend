e2aa64e1d0a2687b9d6a30c1f1817df491aefcd8
unknown <pedrohenriquefdasilva14@gmail.com>
2024-08-22 21:25:32 -0300

    feat: implement findExtractByUseLoggedr method in ExtractController

--- wallet/wallet/src/main/java/com/swiss/wallet/web/controller/ExtractController.java
+++ wallet/wallet/src/main/java/com/swiss/wallet/web/controller/ExtractController.java
@@ -1,6 +1,13 @@
 package com.swiss.wallet.web.controller;
 
+import com.swiss.wallet.entity.Extract;
+import com.swiss.wallet.jwt.JwtUserDetails;
 import com.swiss.wallet.service.ExtractService;
+import com.swiss.wallet.web.dto.ExtractResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +21,10 @@ public class ExtractController {
         this.extractService = extractService;
     }
 
+    @GetMapping("/current")
+    @PreAuthorize("hasRole('CLIENT')")
+    public ResponseEntity<ExtractResponseDto> findExtractByUserLogged(@AuthenticationPrincipal JwtUserDetails userDetails){
+        Extract extract = extractService.findByUserLogged(userDetails.getId());
+        return ResponseEntity.ok().body(ExtractResponseDto.toExtractResponse(extract));
+    }
 }
