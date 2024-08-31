1f42bbaa4b34ca1aad47cbb03d3e4ecb2e096ea3
unknown <pedrohenriquefdasilva14@gmail.com>
2024-08-30 21:16:08 -0300

    feat: create docs in saveProduct

--- src/main/java/com/swiss/wallet/web/controller/ProductController.java
+++ src/main/java/com/swiss/wallet/web/controller/ProductController.java
@@ -6,7 +6,14 @@ import com.swiss.wallet.entity.Product;
 import com.swiss.wallet.service.ProductService;
 import com.swiss.wallet.web.dto.ProductCreateDto;
 import com.swiss.wallet.web.dto.ProductResponseDto;
+import com.swiss.wallet.web.dto.UserResponseDto;
+import com.swiss.wallet.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +32,15 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "Create a new product", description = "Feature to create a new product",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Resource created successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "422", description = "Resource not processed due to invalid input data",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "User not allowed to access this resource",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            })
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ProductResponseDto> saveProduct(
@@ -38,8 +54,9 @@ public class ProductController {
             throw new RuntimeException(e);
         }
         ProductResponseDto responseDto = productService.saveProduct(createDto, file);
-        return ResponseEntity.ok().body(responseDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
+
 
     @GetMapping("/category")
     @PreAuthorize("hasRole('ADMIN') OR hasRole('CLIENT')")
