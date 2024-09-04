0f8edb08ef97e939313af44b90202da48bd6dfdb
unknown <pedrohenriquefdasilva14@gmail.com>
2024-09-04 20:21:10 -0300

    feat: implement saveOrder in OrderService

--- src/main/java/com/swiss/wallet/service/OrderService.java
+++ src/main/java/com/swiss/wallet/service/OrderService.java
@@ -1,5 +1,10 @@
 package com.swiss.wallet.service;
 
+import com.swiss.wallet.entity.Order;
+import com.swiss.wallet.entity.Product;
+import com.swiss.wallet.entity.Status;
+import com.swiss.wallet.entity.UserEntity;
+import com.swiss.wallet.exception.ObjectNotFoundException;
 import com.swiss.wallet.repository.IOrderRepository;
 import com.swiss.wallet.repository.IProductRepository;
 import com.swiss.wallet.repository.IUserRepository;
@@ -20,6 +25,22 @@ public class OrderService {
         this.userRepository = userRepository;
     }
 
+    public Order saveOrder(Long id, Long idProduct) {
+        UserEntity user = userRepository.findById(id)
+                .orElseThrow(
+                        () -> new ObjectNotFoundException(String.format("User not found. Please check the user ID or username and try again."))
+                );
+        Product product = productRepository.findById(idProduct)
+                .orElseThrow(
+                        () -> new ObjectNotFoundException("Product not found, Please check the product ID and try again")
+                );
 
+        Order order = new Order();
+        order.setUser(user);
+        order.setProduct(product);
+        order.setStatus(Status.ANALYSIS);
+        return orderRepository.save(order);
+
+    }
 
 }
