db10012420c6ef79fa3f6021200a9192a44c7b14
unknown <pedrohenriquefdasilva14@gmail.com>
2024-09-04 20:02:13 -0300

    feat: implemente saveFavorite methods in FavoriteController

--- src/main/java/com/swiss/wallet/web/controller/FavoriteController.java
+++ src/main/java/com/swiss/wallet/web/controller/FavoriteController.java
@@ -1,7 +1,13 @@
 package com.swiss.wallet.web.controller;
 
+import com.swiss.wallet.jwt.JwtUserDetails;
 import com.swiss.wallet.service.FavoriteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +20,12 @@ public class FavoriteController {
         this.favoriteService = favoriteService;
     }
 
+    @PostMapping
+    @PreAuthorize("hasRole('CLIENT')")
+    public ResponseEntity<Void> saveFavorite(@AuthenticationPrincipal JwtUserDetails userDetails,
+                                             @RequestParam("idProduct") Long idProduct){
+        favoriteService.saveFavorite(userDetails.getId(), idProduct);
+        return ResponseEntity.ok().build();
+    }
 
 }
