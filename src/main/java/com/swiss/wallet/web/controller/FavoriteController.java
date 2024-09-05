a9cf6551c88648ca7e0f096d4f43cbdb5c05e8b3
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-05 13:09:34 -0300

    feat: document method save favorite

--- src/main/java/com/swiss/wallet/web/controller/FavoriteController.java
+++ src/main/java/com/swiss/wallet/web/controller/FavoriteController.java
@@ -4,6 +4,12 @@ import com.swiss.wallet.entity.Favorite;
 import com.swiss.wallet.jwt.JwtUserDetails;
 import com.swiss.wallet.service.FavoriteService;
 import com.swiss.wallet.web.dto.FavoriteResponseDto;
+import com.swiss.wallet.web.dto.UserResponseDto;
+import com.swiss.wallet.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,6 +29,17 @@ public class FavoriteController {
         this.favoriteService = favoriteService;
     }
 
+    @Operation(summary = "Save a new favorite product", description = "Feature to save a new favorite product",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Resource saved successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Resource not processed due to existing favorite",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Resource not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "User not allowed to access this resource",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            })
     @PostMapping
     @PreAuthorize("hasRole('CLIENT')")
     public ResponseEntity<Void> saveFavorite(@AuthenticationPrincipal JwtUserDetails userDetails,
