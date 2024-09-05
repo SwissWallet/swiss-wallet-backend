9b1dbcf22083c4a5168526635d3ab101977d21de
unknown <pedrohenriquefdasilva14@gmail.com>
2024-09-05 20:54:37 -0300

    feat: implement createUser_WithInvalidZipCode_ReturnErrorMessageStatus422 method to test

--- src/main/java/com/swiss/wallet/web/dto/AddressCreateDto.java
+++ src/main/java/com/swiss/wallet/web/dto/AddressCreateDto.java
@@ -1,11 +1,18 @@
 package com.swiss.wallet.web.dto;
 
 import com.swiss.wallet.entity.Address;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record AddressCreateDto (String zipCode,
+public record AddressCreateDto (@NotBlank
+                                String zipCode,
+                                @NotBlank
                                 String street,
+                                @NotBlank
                                 String city,
+                                @NotNull
                                 Long number,
+                                @NotBlank
                                 String uf) {
 
     public Address toAddress(){
