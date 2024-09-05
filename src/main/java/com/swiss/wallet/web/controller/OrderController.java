20a861026107827c5ccef3c114a34d4fa53107b4
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-05 13:33:45 -0300

    feat: name controller in documentation

--- src/main/java/com/swiss/wallet/web/controller/OrderController.java
+++ src/main/java/com/swiss/wallet/web/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.swiss.wallet.entity.Order;
 import com.swiss.wallet.jwt.JwtUserDetails;
 import com.swiss.wallet.service.OrderService;
 import com.swiss.wallet.web.dto.OrderResponseDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Order", description = "Contains all operations related to resources for registering, editing and reading a order.")
 @RestController
 @RequestMapping("/api/v3/orders")
 public class OrderController {
