7b537ea94580047fa3d69ea77e28fc5c169d09eb
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-05 14:22:16 -0300

    feat: create users insert sql

---
+++ src/test/resources/sql/users/users-insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO `adresses` (`id`, `city`, `number`, `street`, `uf`, `zip_code`)
+VALUES
+(1, 'São Paulo', 123, 'Rua das Flores', 'SP', '01000-000'),
+(2, 'Rio de Janeiro', 456, 'Avenida Atlântica', 'RJ', '22010-000'),
+(3, 'Belo Horizonte', 789, 'Praça Sete de Setembro', 'MG', '30110-000');
+
+INSERT INTO `users` (`id`, `birth_date`, `cpf`, `name`, `password`, `phone`, `role`, `username`, `verification_code`, `id_address`)
+VALUES
+(1, '1990-01-01', '08422365004', 'João Silva', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '11987654321', 'ROLE_ADMIN', 'joao@email.com', null, 1),
+(2, '1985-05-15', '58793541090', 'Maria Oliveira', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '21998765432', 'ROLE_CLIENT', 'maria@email.com', null, 2),
+(3, '1992-08-30', '34861623079', 'Carlos Pereira', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '31987654321', 'ROLE_CLIENT', 'carlos@email.com', null, 3);
