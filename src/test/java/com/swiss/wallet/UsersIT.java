09b53cea4d299ce010565a6f014e28687039d547
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-05 15:30:23 -0300

    feat: impelement createUser_WithInvalidUsername_ReturnErrorMessageStatus422 method to test

--- src/test/java/com/swiss/wallet/UsersIT.java
+++ src/test/java/com/swiss/wallet/UsersIT.java
@@ -5,6 +5,7 @@ import com.swiss.wallet.web.dto.AddressCreateDto;
 import com.swiss.wallet.web.dto.UserAddressCreateDto;
 import com.swiss.wallet.web.dto.UserCreateDto;
 import com.swiss.wallet.web.dto.UserResponseDto;
+import com.swiss.wallet.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -55,5 +56,137 @@ public class UsersIT {
         org.assertj.core.api.Assertions.assertThat(responseDto.id()).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseDto.username()).isEqualTo("tobby@email.com");
         org.assertj.core.api.Assertions.assertThat(responseDto.role()).isEqualTo(Role.ROLE_CLIENT);
+    }
+
+    @Test
+    public void createUser_WithInvalidUsername_ReturnErrorMessageStatus422(){
+        ErrorMessage responseDto = testClient
+                .post()
+                .uri("/api/v3/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(
+                        new UserAddressCreateDto(
+                                new UserCreateDto(
+                                        "",
+                                        "123456",
+                                        "Tobby Henrique",
+                                        "89018551007",
+                                        "20/05/2001",
+                                        "11987654321"
+                                ),
+                                new AddressCreateDto(
+                                        "01000-000",
+                                        "Alameda Joaquina",
+                                        "Taboão da Serra",
+                                        18L,
+                                        "SP"
+                                )
+                        )
+                )
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseDto).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseDto.getStatus()).isEqualTo(422);
+
+        responseDto = testClient
+                .post()
+                .uri("/api/v3/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(
+                        new UserAddressCreateDto(
+                                new UserCreateDto(
+                                        "tobby@email",
+                                        "123456",
+                                        "Tobby Henrique",
+                                        "89018551007",
+                                        "20/05/2001",
+                                        "11987654321"
+                                ),
+                                new AddressCreateDto(
+                                        "01000-000",
+                                        "Alameda Joaquina",
+                                        "Taboão da Serra",
+                                        18L,
+                                        "SP"
+                                )
+                        )
+                )
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseDto).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseDto.getStatus()).isEqualTo(422);
+
+        responseDto = testClient
+                .post()
+                .uri("/api/v3/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(
+                        new UserAddressCreateDto(
+                                new UserCreateDto(
+                                        "tobby@",
+                                        "123456",
+                                        "Tobby Henrique",
+                                        "89018551007",
+                                        "20/05/2001",
+                                        "11987654321"
+                                ),
+                                new AddressCreateDto(
+                                        "01000-000",
+                                        "Alameda Joaquina",
+                                        "Taboão da Serra",
+                                        18L,
+                                        "SP"
+                                )
+                        )
+                )
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseDto).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseDto.getStatus()).isEqualTo(422);
+
+        responseDto = testClient
+                .post()
+                .uri("/api/v3/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(
+                        new UserAddressCreateDto(
+                                new UserCreateDto(
+                                        "tobby",
+                                        "123456",
+                                        "Tobby Henrique",
+                                        "89018551007",
+                                        "20/05/2001",
+                                        "11987654321"
+                                ),
+                                new AddressCreateDto(
+                                        "01000-000",
+                                        "Alameda Joaquina",
+                                        "Taboão da Serra",
+                                        18L,
+                                        "SP"
+                                )
+                        )
+                )
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseDto).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseDto.getStatus()).isEqualTo(422);
+
     }
 }
--- src/test/resources/sql/users/users-insert.sql
+++ src/test/resources/sql/users/users-insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO `adresses` (`id`, `city`, `number`, `street`, `uf`, `zip_code`)
 VALUES
-(4, 'São Paulo', 123, 'Rua das Flores', 'SP', '01000-000'),
-(5, 'Rio de Janeiro', 456, 'Avenida Atlântica', 'RJ', '22010-000'),
-(6, 'Belo Horizonte', 789, 'Praça Sete de Setembro', 'MG', '30110-000');
+(400, 'São Paulo', 123, 'Rua das Flores', 'SP', '01000-000'),
+(500, 'Rio de Janeiro', 456, 'Avenida Atlântica', 'RJ', '22010-000'),
+(600, 'Belo Horizonte', 789, 'Praça Sete de Setembro', 'MG', '30110-000');
 
 INSERT INTO `users` (`id`, `birth_date`, `cpf`, `name`, `password`, `phone`, `role`, `username`, `verification_code`, `id_address`)
 VALUES
-(4, '1990-01-01', '08422365004', 'João Silva', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '11987654321', 'ROLE_ADMIN', 'joao@email.com', null, 4),
-(5, '1985-05-15', '58793541090', 'Maria Oliveira', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '21998765432', 'ROLE_CLIENT', 'maria@email.com', null, 5),
-(6, '1992-08-30', '34861623079', 'Carlos Pereira', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '31987654321', 'ROLE_CLIENT', 'carlos@email.com', null, 6);
+(400, '1990-01-01', '08422365004', 'João Silva', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '11987654321', 'ROLE_ADMIN', 'joao@email.com', null, 400),
+(500, '1985-05-15', '58793541090', 'Maria Oliveira', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '21998765432', 'ROLE_CLIENT', 'maria@email.com', null, 500),
+(600, '1992-08-30', '34861623079', 'Carlos Pereira', '$2a$12$0mTx/SSBjfscwepDDBuJ3uefaDiz8DUjgKlAOAtoV1YbLcOFmh.a6', '31987654321', 'ROLE_CLIENT', 'carlos@email.com', null, 600);
