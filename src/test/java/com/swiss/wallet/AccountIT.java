0c7677a642f19af32c5954bed42484f056348f2d
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-08 19:41:33 -0300

    feat: implement getAccountCurrent_WithValidToken_ReturnAccountResponseDtoStatus200 method for test

--- src/test/java/com/swiss/wallet/AccountIT.java
+++ src/test/java/com/swiss/wallet/AccountIT.java
@@ -1,5 +1,8 @@
 package com.swiss.wallet;
 
+import com.swiss.wallet.entity.Role;
+import com.swiss.wallet.web.dto.AccountResponseDto;
+import com.swiss.wallet.web.dto.ResponseGlobalDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,5 +17,34 @@ public class AccountIT {
     @Autowired
     WebTestClient testClient;
 
+    @Test
+    public void getAccountCurrent_WithValidToken_ReturnAccountResponseDtoStatus200(){
+        AccountResponseDto responseDto = testClient
+                .get()
+                .uri("/api/v3/accounts/current")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "joao@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(AccountResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseDto).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseDto.id()).isEqualTo(400);
+        org.assertj.core.api.Assertions.assertThat(responseDto.value()).isEqualTo(0);
+
+        responseDto = testClient
+                .get()
+                .uri("/api/v3/accounts/current")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "maria@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(AccountResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseDto).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseDto.id()).isEqualTo(500);
+        org.assertj.core.api.Assertions.assertThat(responseDto.value()).isEqualTo(0);
+
+    }
 
 }
