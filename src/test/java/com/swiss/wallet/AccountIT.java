7d947d88e23d59e58bf45974a8e77ea2ba154f0e
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-08 19:34:31 -0300

    feat: create AccountIT class for text

---
+++ src/test/java/com/swiss/wallet/AccountIT.java
@@ -0,0 +1,18 @@
+package com.swiss.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/users/users-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class AccountIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+
+}
