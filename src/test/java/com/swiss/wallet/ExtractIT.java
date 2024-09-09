2bdecadb93230ffa920406d4a5b740dc555f30d4
unknown <pedrohenriquefdasilva14@gmail.com>
2024-09-09 20:16:10 -0300

    feat: implement findExtractByUserLogged_WithValidToken_ReturnExtractResponseDtoStatus200 for test

--- src/test/java/com/swiss/wallet/ExtractIT.java
+++ src/test/java/com/swiss/wallet/ExtractIT.java
@@ -1,11 +1,14 @@
 package com.swiss.wallet;
 
-import com.swiss.wallet.web.dto.AccountResponseDto;
+import com.swiss.wallet.service.AccountService;
+import com.swiss.wallet.web.dto.ExtractResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -14,6 +17,9 @@ public class ExtractIT {
 
     @Autowired
     WebTestClient testClient;
+
+    @Autowired
+    private AccountService accountService;
 
     @Test
     public void findExtractByUserLogged_WithValidToken_ReturnStatus204(){
@@ -32,4 +38,21 @@ public class ExtractIT {
                 .expectStatus().isNoContent();
     }
 
+
+    @Test
+    public void findExtractByUserLogged_WithValidToken_ReturnExtractResponseDtoStatus200(){
+        accountService.registerDeposit("carlos@email.com", 100D);
+        List<ExtractResponseDto> responseDto = testClient
+                .get()
+                .uri("/api/v3/extracts/current")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "carlos@email.com", "123456"))
+                .exchange()
+                .expectBodyList(ExtractResponseDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseDto).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseDto.get(0).value()).isEqualTo(100);
+
+    }
 }
