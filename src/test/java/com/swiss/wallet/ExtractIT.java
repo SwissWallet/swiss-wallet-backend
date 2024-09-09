20d12a4a04f69997143fefa9534b122997571e7b
unknown <pedrohenriquefdasilva14@gmail.com>
2024-09-09 19:43:52 -0300

    feat: create ExtractIT for test

---
+++ src/test/java/com/swiss/wallet/ExtractIT.java
@@ -0,0 +1,19 @@
+package com.swiss.wallet;
+
+import com.swiss.wallet.web.dto.AccountResponseDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/users/users-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class ExtractIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+
+}
