755ed23374961aea871db2cdf93d8b30fda8a20a
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-09 12:26:16 -0300

    feat: create AUthenticationIT class for test

---
+++ src/test/java/com/swiss/wallet/AuthenticationIT.java
@@ -0,0 +1,16 @@
+package com.swiss.wallet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/users/users-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class AuthenticationIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+}
