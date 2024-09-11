168f6bdc9f2ba786e830cffa15106e66242c81bb
unknown <pedrohenriquefdasilva14@gmail.com>
2024-09-11 19:41:31 -0300

    feat: implement savePurchase method in PurchaseController

--- src/main/java/com/swiss/wallet/service/PurchaseService.java
+++ src/main/java/com/swiss/wallet/service/PurchaseService.java
@@ -1,7 +1,13 @@
 package com.swiss.wallet.service;
 
+import com.swiss.wallet.entity.*;
+import com.swiss.wallet.exception.ObjectNotFoundException;
+import com.swiss.wallet.exception.PointInsufficientException;
 import com.swiss.wallet.repository.*;
+import com.swiss.wallet.web.dto.PurchaseCreateDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PurchaseService {
@@ -20,5 +26,55 @@ public class PurchaseService {
         this.iAccountRepository = iAccountRepository;
         this.iExtractRepository = iExtractRepository;
         this.orderRepository = orderRepository;
+    }
+
+    public float calcValueTotal(List<Long> productIds){
+        List<Product> products = iProductRepository.findAllById(productIds);
+        float total = 0.0f;
+        for (Product product : products){
+            total += product.getValue();
+        }
+        return total;
+    }
+
+    public Purchase savePurchase(PurchaseCreateDto purchaseCreateDto) {
+
+        float value = calcValueTotal(purchaseCreateDto.productIds());
+
+        UserEntity user = userRepository.findByUsername(purchaseCreateDto.userUsername())
+                .orElseThrow(
+                        () -> new ObjectNotFoundException(String.format("User username= %s not found", purchaseCreateDto.userUsername()))
+                );
+
+        List<Product> products = iProductRepository.findAllById(purchaseCreateDto.productIds());
+        Account account = iAccountRepository.findAccountByUser(user).orElseThrow(
+                () -> new ObjectNotFoundException(String.format("Account id = %s not found", user.getName()))
+        );
+
+        if (account.getValue() < value){
+            throw new PointInsufficientException("Insufficient ponint balance");
+        }
+        account.setValue(account.getValue() - value);
+        iAccountRepository.save(account);
+
+        Purchase purchase = new Purchase();
+        purchase.setUser(user);
+        purchase.setValue(value);
+        purchase.setProducts(products);
+        purchase = iPurchaseRepository.save(purchase);
+
+        Extract extract = new Extract();
+        extract.setAccount(account);
+        extract.setValue((double) value);
+        extract.setType(Extract.Type.TRANSACTION);
+        extract.setDescription("Points transaction");
+        iExtractRepository.save(extract);
+
+        List<Order> orders = orderRepository.findAllByUserAndProductIn(user, products);
+        orders.stream()
+                .forEach(order -> order.setStatus(Status.COMPLETED));
+        orderRepository.saveAll(orders);
+
+        return purchase;
     }
 }
