3574f11a928e9a84a238dcdb73b8ea90f67a6c70
unknown <pedrohenriquefdasilva14@gmail.com>
2024-09-12 18:55:21 -0300

    feat: add tag for documentation

--- src/main/java/com/swiss/wallet/web/controller/PurchaseController.java
+++ src/main/java/com/swiss/wallet/web/controller/PurchaseController.java
@@ -4,6 +4,7 @@ import com.swiss.wallet.entity.Purchase;
 import com.swiss.wallet.service.PurchaseService;
 import com.swiss.wallet.web.dto.PurchaseCreateDto;
 import com.swiss.wallet.web.dto.PurchaseResponseDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Purchase", description = "Contains all operations related to resources for registering, editing and reading a purchase.")
 @RestController
 @RequestMapping("/api/v3/purchases")
 public class PurchaseController {
