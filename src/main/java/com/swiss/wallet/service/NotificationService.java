cd52d54c595c07c35a6bb910bedba02fc8cf832f
unknown <pedrohenriquefdasilva14@gmail.com>
2024-10-07 20:47:46 -0300

    feat: implement method sendNotification in method registerDeposit

--- src/main/java/com/swiss/wallet/service/NotificationService.java
+++ src/main/java/com/swiss/wallet/service/NotificationService.java
@@ -3,10 +3,22 @@ package com.swiss.wallet.service;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.swiss.wallet.entity.TokenNotification;
+import com.swiss.wallet.entity.UserEntity;
+import com.swiss.wallet.repository.ITokenRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NotificationService {
+
+    private final ITokenRepository iTokenRepository;
+
+    public NotificationService(ITokenRepository iTokenRepository) {
+        this.iTokenRepository = iTokenRepository;
+    }
+
 
     public void sendNotification(String token, String title, String body) {
         Message message = Message.builder()
@@ -24,4 +36,9 @@ public class NotificationService {
         }
     }
 
+    public List<TokenNotification> findAllByUser(UserEntity user) {
+
+        return iTokenRepository.findAllByUser(user);
+
+    }
 }
